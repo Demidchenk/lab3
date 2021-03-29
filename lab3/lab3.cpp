e22235c792c33e9f3e6460ef92dfc84d01c942d4
Alexey Demidchenko <alexey.demidchenko@gmail.com>
2021-03-29 09:28:20 +0300

    commit

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -35,6 +35,18 @@ int main()
     while (!inFile.eof())
     {
         getline(inFile, map[i], '\n');
+        cout << map[i] << endl;
         i++;
     }
+
+    int x1, y1, x2, y2;
+    cout << "Input starting coordinates\n";
+    cin >> x1, y1;
+    cout << "Input ending coordinates\n";
+    cin >> x2, y2;
+
+    int mark[30][30] = { 10000 };
+    int x3 = x1, y3 = y1;
+    
+    
 }
